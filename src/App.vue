b5ec12f970a07043d8f28361c7e3730b66f578da
Greg Desplaces <contact@gregdesplaces.com>
2024-04-25 15:35:53 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+ <div class="container">
+   <header>En tête</header>
+   <hr>
+   <main> Corps de l'app</main>
+   <hr>
+   <footer>Pied de page</footer>
+ </div>
+</template>
+
+<style scoped>
+
+</style>
